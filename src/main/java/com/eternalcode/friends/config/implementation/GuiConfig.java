8d2f25ffeedcd80e3c1439bb83604b4a4fcab214
Kamil Jęczeń <kamicjusz.kontakt@gmail.com>
2022-09-25 20:50:13 +0200

    GH-4 Add gui configuration file (#4)
    * add gui configuration file
    add gui initialization
    
    * reconstruction of items declarations
    
    * move Item creation from ItemGuiBuilder to ConfigItem class
    
    * rename get method in ConfigItem to toGuiItem
    add description to config values
    
    * add iteration of ConfigItems at main gui initialization
    
    * add iteration of ConfigItems at main gui initialization
    
    * reconstruction of item list in main gui initialization

---
+++ src/main/java/com/eternalcode/friends/config/implementation/GuiConfig.java
@@ -0,0 +1,72 @@
+package com.eternalcode.friends.config.implementation;
+
+import com.eternalcode.friends.config.ReloadableConfig;
+import com.eternalcode.friends.util.legacy.Legacy;
+import dev.triumphteam.gui.builder.item.ItemBuilder;
+import dev.triumphteam.gui.guis.GuiItem;
+import net.dzikoysk.cdn.entity.Contextual;
+import net.dzikoysk.cdn.entity.Description;
+import net.dzikoysk.cdn.source.Resource;
+import net.dzikoysk.cdn.source.Source;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class GuiConfig implements ReloadableConfig {
+
+    public MainGui mainGui = new MainGui();
+
+    @Override
+    public Resource resource(File folder) {
+        return Source.of(folder, "gui.yml");
+    }
+
+    public ConfigItem friendListItem = new ConfigItem();
+    public ConfigItem receivedAndSentInvitesItem = new ConfigItem();
+    public ConfigItem sendInvitesItem = new ConfigItem();
+    public ConfigItem settingItem = new ConfigItem();
+
+    @Contextual
+    public static class MainGui {
+        @Description("# Rows of inventory (up to 6)")
+        public int rows = 3;
+
+        @Description("# Title of inventory")
+        public String title = "&bFriends";
+    }
+
+    @Contextual
+    public static class ConfigItem {
+
+        public int slot = 0;
+        @Description("# Material from https://hub.spigotmc.org/javadocs/bukkit/org/bukkit/Material.html")
+        public Material type = Material.STONE;
+
+        public String name = "&fItem name";
+
+        @Description("# Description of item")
+        public List<String> lore = Arrays.asList("&fFirst line of lore", "&9Second line of lore");
+
+        @Description("# Item should be enchanted or not")
+        public boolean enchanted = false;
+
+        public GuiItem toGuiItem() {
+            ItemBuilder builder = ItemBuilder.from(this.type)
+                    .name(Legacy.component(this.name))
+                    .lore(this.lore.stream().map(Legacy::component).collect(Collectors.toList()))
+                    .flags(ItemFlag.HIDE_ATTRIBUTES)
+                    .flags(ItemFlag.HIDE_ENCHANTS);
+            if (this.enchanted) {
+                builder.enchant(Enchantment.LOOT_BONUS_BLOCKS);
+            }
+            return builder.asGuiItem();
+        }
+    }
+}
--- src/main/java/com/eternalcode/friends/gui/MainGUI.java
+++ src/main/java/com/eternalcode/friends/gui/MainGUI.java
@@ -1,38 +1,54 @@
 package com.eternalcode.friends.gui;
 
+import com.eternalcode.friends.config.implementation.GuiConfig;
 import dev.triumphteam.gui.builder.item.ItemBuilder;
 import dev.triumphteam.gui.guis.Gui;
 import dev.triumphteam.gui.guis.GuiItem;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemFlag;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainGUI {
 
     private final MiniMessage miniMessage;
+    private final GuiConfig guiConfig;
 
-    private final String mainInventoryTitle = "Friends";
-    private final String friendListItemName = "Friend List";
+    private Material type;
+
+    private Gui gui;
 
 
-    public MainGUI(MiniMessage miniMessage) {
+    public MainGUI(MiniMessage miniMessage, GuiConfig guiConfig) {
         this.miniMessage = miniMessage;
+        this.guiConfig = guiConfig;
+        initializeGui();
     }
 
-    public void openInventory(Player player) {
-
-        GuiItem friendList = ItemBuilder.from(Material.BOOK)
-                .name(this.miniMessage.deserialize("&a" + this.friendListItemName))
-                .asGuiItem();
-
-        Gui gui = Gui.gui()
-                .title(this.miniMessage.deserialize(mainInventoryTitle))
-                .rows(3)
+    private void initializeGui() {
+        GuiConfig.MainGui mainGui = guiConfig.mainGui;
+        this.gui = Gui.gui()
+                .title(this.miniMessage.deserialize(mainGui.title))
+                .rows(mainGui.rows)
                 .disableItemTake()
                 .create();
 
-        gui.setItem(2, 3, friendList);
+        List.of(
+                guiConfig.friendListItem,
+                guiConfig.receivedAndSentInvitesItem,
+                guiConfig.sendInvitesItem,
+                guiConfig.settingItem
+        ).forEach(item -> this.gui.setItem(item.slot, item.toGuiItem()));
+    }
 
+    public void openInventory(Player player) {
         gui.open(player);
     }
 }
